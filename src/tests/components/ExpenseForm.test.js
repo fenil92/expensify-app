7ce6053e129f763e1a0f68ab67431bf7dcb3e1ed
Fenil Patel <fenilpatel269@gmail.com>
2019-08-04 02:26:36 +0530

    test spy

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,39 @@ test('should render expense form with expense data', () => {
     const wrapper = shallow(<ExpenseForm expense ={expenses[1]} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error on invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm/>);
+    wrapper.find('form').simulate('submit', {
+        preventDefault : () => {}
+    });
+
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+    const value = "New description";
+    const wrapper = shallow(<ExpenseForm/>);
+    wrapper.find('input').at(0).simulate('change', {
+        target: {value}
+    });
+
+    expect(wrapper.state('description')).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should call onSubmit prop for valid form submission', () => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense={expenses[2]} onSubmit={onSubmitSpy} />);
+    wrapper.find('form').simulate('submit', {
+        preventDefault : () => {}
+    });
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenCalledWith({
+        description : expenses[2].description,
+        note : expenses[2].note,
+        amount : expenses[2].amount,
+        createdAt : expenses[2].createdAt
+    });
+});
